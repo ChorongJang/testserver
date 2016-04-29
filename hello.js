e656afe1ff3251fc3c4558e737116b6e30e5e4c0
crjang <crjang91@gmail.com>
2016-04-29 22:10:44 +0900

    init project

---
+++ hello.js
@@ -0,0 +1,6 @@
+
+
+var colors = require('colors');
+
+console.log('hello world');
+console.log('hello'.red);
---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res){
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+
+	res.end('<h1>  HELLO </h1>')
+
+}).listen(8080, function(){
+
+	 console.log('create server');
+	});
